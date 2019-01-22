6559ef7301aa0c6cb3892f0fc72a593d465de1b2
chuang <chuang@whitepages.com>
2019-01-21 20:51:04 -0800

    Updates

---
+++ precache-manifest.63d5d54a18e1434035840c4fbfbbb47e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d1248d20b016b2fcd34c",
+    "url": "/static/js/main.d1248d20.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "d1248d20b016b2fcd34c",
+    "url": "/static/css/main.68874c75.chunk.css"
+  },
+  {
+    "revision": "866265f9b370bd23722d7186eb50f49c",
+    "url": "/index.html"
+  }
+];
